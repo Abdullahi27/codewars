2c48f1a553e9a2029cbc1c4c7625ffc9a7b9549e
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2022-09-25 20:44:20 -0400

    new challenge

---
+++ 8kyu/divisibleBy.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/55edaba99da3a9c84000003b
+
+//Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. 
+//First argument is an array of numbers and the second is the divisor.
+
+
+function divisibleBy(numbers, divisor){
+
+    return numbers.filter(item => item % divisor === 0 )
+}
