a1eede9d394cc1f05f805d9fdc73521c3e36b0e3
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2022-10-12 22:55:15 -0400

    new challenges

---
+++ 8kyu/stringToNum.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+
+const stringToNumber = function(str){
+    return +(str)
+  }
+  
+  // We need a function that can transform a string into a number.
---
+++ 8kyu/willYouMakeIt.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5861d28f124b35723e00005e/train/javascript
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    if (mpg * fuelLeft >= distanceToPump) {
+      return true
+    }else {
+     return false
+    }
+  };
+  
+  // You were camping with your friends far away from home, 
+  // but when it's time to go back, you realize that your fuel 
+  // is running out and the nearest pump is 50 miles away! 
+  // You know that on average, your car runs on about 
+  // 25 miles per gallon. There are 2 gallons left.
