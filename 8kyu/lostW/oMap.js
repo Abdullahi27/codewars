afa93b20e964394b669334f18d75dcd07e308597
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2022-10-22 21:08:23 -0400

    daily challenge

---
+++ 8kyu/lostW/oMap.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+
+function maps(x){
+    let newX = [];
+    for (let i=0; i<x.length; i++){
+     newX.push(x[i]*2)
+    }
+    return newX
+  }
+  
+  // Given an array of integers, return a new array with each value doubled.
