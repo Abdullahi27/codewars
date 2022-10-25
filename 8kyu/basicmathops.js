0cfb984e855cd2ad59b18469e31f63d2cd82e765
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2022-10-24 22:38:08 -0400

    daily challenge

---
+++ 8kyu/basicmathops.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/javascript
+
+function basicOp(operation, value1, value2){  
+    if (operation == '+') {
+      return value1 + value2
+    } else if (operation == '-') {
+      return value1 - value2
+    }else if (operation == '*') {
+      return value1 * value2
+    } else if (operation == '/') {
+      return value1 / value2
+    } 
+    
+  }
+  
+  // example  ('+', 4, 7) --> 11
