7503af426618ec7b48bbd299099bdd2001c5f095
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2022-10-25 23:04:16 -0400

    daily challenge

---
+++ 8kyu/makeUpperCase.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
+  
+  // Write a function which converts the input string to uppercase.
