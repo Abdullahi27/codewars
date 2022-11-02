550aaf3de74aa5e51f442cb5e44e409997913a3f
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2022-11-01 22:15:43 -0400

    daily challenge

---
+++ 8kyu/meanofanArray.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/563e320cee5dddcf77000158/train/javascript
+
+function getAverage(marks){
+    let av = 0
+    for (let i=0; i<marks.length; i++){
+       av += marks[i]
+    }
+       return Math.floor(av/marks.length)
+   }
+   
+   // Return the average of the given array rounded down to its nearest integer.
