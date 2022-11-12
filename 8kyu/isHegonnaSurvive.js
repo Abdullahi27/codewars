b244b84191ce96116cef655e60cfb96e40c6aca2
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2022-11-11 22:13:36 -0500

    daily challenge

---
+++ 8kyu/isHegonnaSurvive.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+function hero(bullets, dragons){
+    if (bullets >= dragons *2) {
+      return true
+    }else {
+      return false
+    }
+  }
+  
+  // is bullets 2 times greaters than the amount of dragons if so true otherwise nah 
