9986f8549bac84f567dbef31cd152ae90865ba02
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2022-11-16 20:57:52 -0500

    daily challenge

---
+++ 8kyu/areYouPlayingBanjo?.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/53af2b8861023f1d88000832/train/javascript
+function areYouPlayingBanjo(name) {
+    return name.charAt(0).toUpperCase() == 'R' ? name + " plays banjo"  : name + " does not play banjo" 
+ 
+ }
+ 
+ // If your name starts with the letter "R" or lower case "r", 
+ // you are playing banjo!
